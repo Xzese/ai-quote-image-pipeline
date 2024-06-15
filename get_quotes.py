9556f52b00deb42a8b66e4d01406aa1eff4f6977
Xzese <Xzese@users.noreply.github.com>
2024-06-15 14:49:59 +0100

    Added code to get quotes using an api

---
+++ get_quotes.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import os
+
+endpoint_url = "https://api.quotable.io/quotes"
+file_path = "output.json"
+quote_list = []
+total_pages = 1
+current_page = 1
+
+params = {
+    "limit": 150
+}
+
+while current_page <= total_pages:
+    print("Page Number: " + str(current_page))
+    response = requests.get(endpoint_url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        quote_list += data['results']
+        total_pages = data['totalPages']
+        current_page += 1
+    else:
+        print(f"Failed to retrieve data. HTTP Status code: {response.status_code}")
+
+with open(file_path, 'w') as json_file:
+    json.dump(quote_list, json_file, indent=4)
